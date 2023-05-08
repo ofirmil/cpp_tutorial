ab60edad722e8fa3240e78857aea0ca20dc48539
Ofir Miller <ofir.miller@argus-sec.com>
2023-05-08 15:18:02 +0300

    working use of library with python script

--- robot_brain.py
+++ robot_brain.py
@@ -1,22 +1,47 @@
-import time import robot_brain
+import time
+import robot_brain
 
-    def Log(robot_positions, motors_positions) :print(f "robot_positions: {len(robot_positions)}") print(f "motors_positions: {len(motors_positions)}") if len (robot_positions)> 0 :print(f "first robot pos {robot_positions[0]}") print(f "last robot pos {robot_positions[len(robot_positions) - 1]}") return robot_brain.VectorDouble([3.1, 3.2, 3.3, 3.4, 3.5])
+motors_positions_cache = []
+def Log(motors_positions, robot_positions):
+    print(f"robot_positions: {len(robot_positions)}")
+    print(f"motors_positions: {len(motors_positions)}")
+    if len(robot_positions) > 0:
+        print(f"first robot pos {robot_positions[0]}")
+        print(f"last robot pos {robot_positions[len(robot_positions) - 1]}")
+    return robot_brain.VectorDouble([3.1, 3.2, 3.3, 3.4, 3.5])
 
-#res = robot_brain.add(7, 3)
-#print(f "robot_brain.add(7, 3) = {res}")
 
-#pet = robot_brain.Pet("Dog")
-#print(pet.getName())
-#pet.setName("Cat")
-#print(pet.getName())
+def SaveCacheAndClear(motors_positions, robot_positions):
+    print(f"robot_positions: {len(robot_positions)}")
+    print(f"motors_positions: {len(motors_positions)}")
+    if len(motors_positions) > 0:
+        motors_positions.clear()
+    if len(robot_positions) > 0:
+        robot_positions.clear()
+    SaveCacheAndClear.counter += 1.0
+    return robot_brain.VectorDouble([SaveCacheAndClear.counter] * 5)
+SaveCacheAndClear.counter = 0.0
 
-                                                                                                                                                                                                                                                                                                                                                robot = robot_brain.brain_t("tests/config_test.yaml") robot.set_net_function("LOG", Log) robot.set_active_net_function("LOG") robot.run() time.sleep(5) robot.stop()
+robot = robot_brain.brain_t("tests/config_test.yaml")
 
-#robot = robot_brain.brain_t("tests/config_test.yaml")
+robot.run()
+time.sleep(2)
 
-#def log_net_function(vector2_of_double_ref, vector_pos_double):
-#print(vector2_of_double_ref)
-#print(vector_pos_double)
-#vector2_of_double_ref[0] = 5.5
+print("### Set log function for first time ###")
+robot.set_net_function("LOG", Log)
+robot.set_active_net_function("LOG")
+time.sleep(2)
 
-#robot.set_net_function("INCREMENT_BY_2", log_net_function)
+print("### Set log function for second time ###")
+robot.set_net_function("LOG", Log)
+
+time.sleep(2)
+
+robot.set_net_function("INCREMENT_BY_2", SaveCacheAndClear)
+# print("### Setting  SaveCacheAndClear() ###")
+# robot.set_active_net_function("INCREMENT_BY_2")
+# time.sleep(3)
+
+# print("### stop() ###")
+robot.stop()
+print("### stop() Done ###")
